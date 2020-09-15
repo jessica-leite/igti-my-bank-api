35c341c0a1e8593c4ac6d768408b07557e4372bc
Jessica Leite <jessica.mleite@gmail.com>
2020-09-14 22:05:17 -0300

    account post endpoint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 import express from 'express';
+import fs from 'fs';
+import { promisify } from 'util';
 
 const app = express();
+const exists = promisify(fs.exists);
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+
+global.fileName = 'accounts.json';
+
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000);
+app.post('/account', async (req, res) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await readFile(global.fileName, 'utf8'));
+        account = { id: data.nextId++, ...account };
+        data.accounts.push(account);
+        await writeFile(global.fileName, JSON.stringify(data));
+
+        res.end();
+    } catch (err) {
+        res.status(400).send({ error: err.message });
+    }
+});
+
+
+app.listen(3000, async () => {
+    try {
+        const fileExists = await exists(global.fileName);
+        if (!fileExists) {
+            const initialJson = {
+                nextId: 1,
+                accounts: []
+            };
+            await writeFile(global.fileName, JSON.stringify(initialJson));
+        }
+    } catch (err) {
+        logger.error(err);
+    }
+});
